94062964eb498d3cab98377c55d3e4874dc1bbb4
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-04-12 19:44:35 -0400

    added IO project

---
+++ Patterns.IO/FileMetadata.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Patterns.IO
+{
+    public class FileMetadata
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public string Id { get; set; }
+        /// <summary>
+        /// Gets or sets the type of the MIME.
+        /// </summary>
+        /// <value>The type of the MIME.</value>
+        public string MimeType { get; set; }
+    }
+}
---
+++ Patterns.IO/FileQuery.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Patterns.IO
+{
+    public class FileQuery
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public string Id { get; set; }
+        /// <summary>
+        /// Gets or sets the file path.
+        /// </summary>
+        /// <value>The file path.</value>
+        public string FilePath { get; set; }
+    }
+}
